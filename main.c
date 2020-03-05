22dbe25cf6666988763bf2b1b73c73d957282a84
STOJILOVIC Ivan <Ivan.STOJILOVIC@cpnv.ch>
2020-03-05 16:52:40 +0100

    work in progress

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+
 int main() {
-    printf("Hello, World!\n");
+
+    printf("\nBienvenue a la Bataille Navale");
+    printf("\n\nVoici l'aide du jeu:");
+    printf("\n\nLe jeu possde 5 navires: \n1 portes avion (5 cases),"
+           " \n1 croiseur (4 cases), \n2 contre-torpilleurs (3 cases),"
+           " \n1 torpilleur (2 cases)."
+           "\nLe but du jeu est de rentrer des coordonnee (par example B4) \net d'essayer de toucher"
+           " les bateaux adverses. En tirant le moins de coup possible.");
+
+    printf("\n--------------------------");
+    printf("\n|    |    |    |    |    |");
+
     return 0;
 }
