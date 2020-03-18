8aacef3be6ba1bc4aff055705436b4f19b6b9db3
unknown <ivan.stojilovic@cpnv.ch>
2020-03-18 11:26:24 +0100

    Version 0.1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DemoGitMA20
+Fichier pour le jeu: Bataille Navale.
+Fait entièrement en code C
+
+Pour l'instant c'est version 0.1.
+Qui permet de jouer ainsi que de demander de l'aide
+et quitter le programme en cours
--- main.c
+++ main.c
@@ -1,29 +1,26 @@
 /**
  *Projet: Jeu Bataille Navale
  *Créateur: Stojilovic Ivan
- *Date:
+ *Date:18.03.2020
  *Version: 0.1
- *
  */
 #include <stdio.h>
 #include <windows.h>
 
 int main() {
-    //console pour reconnaître les accents
     SetConsoleOutputCP(CP_UTF8);
 
 
     //initialiser les valeurs au début
-    int a = 0;
-    int b = 0;
-    int c = 0;
     int i = 0;
     int j = 0;
     int PosH = 0;
     int PosV = 0;
     int bateauCoule = 0;
 
-    //créer le tableau pour notre grille de jeu
+    //créer les tableaux pour notre grille de jeu
+
+    //tableau avec les bateaux vu par le programme
     int carteBateau[10][10] = {
             {0,0,0,0,0,0,0,0,0,0},
             {0,3,3,3,0,0,0,0,0,0},
@@ -37,6 +34,7 @@ int main() {
             {0,0,5,0,0,0,0,0,0,0},
             };
 
+    //tableau vu par le joueur
     int carteJoueur[10][10] = {
             {0,0,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0,0},
@@ -56,14 +54,11 @@ int main() {
            "=================================\n\n");
     printf("Etes-vour prêt ?\n"
            "================\n");
-
     system("Pause");
 
-
-
-
+    //boucle pour pouvoir jouer avec entant de coup que l'on a besoin avant de toucher tout les bateaux
     do {
-
+        //montre la grille au joueur pour s'orienter
         printf("\n      1   2   3   4   5   6   7   8   9   10 \n"
                "    -----------------------------------------\n");
         for (i = 0; i < 10; i++) {
@@ -74,82 +69,95 @@ int main() {
             printf("\n");
             printf("    -----------------------------------------\n");
         }
-        printf("bateau coulé :%d",bateauCoule);
 
-    do {
-        printf("\n\nEntrer la position de tir");
-        printf("\n(Pour quitter taper 11. Ou pour de l'aide taper 12.)");
-        printf("\n\nPosition Horizontal:");
-        scanf("\n%d", &PosH);
-        printf("\nPosition Vertical:");
-        scanf("%d", &PosV);
-        PosH -= 1;
-        PosV -= 1;
-        if ((PosV == 10) || (PosH == 10)) {
-            exit(EXIT_SUCCESS);
-        } else if ((PosV == 11) || (PosH == 11)) {
-            printf("Voici l'aide du jeu:");
-            printf("\n\nLe jeu possede 5 navires: "
-                   "\n1 portes avion (5 cases),"
-                   " \n1 croiseur (4 cases), "
-                   "\n2 contre-torpilleurs (3 cases),"
-                   " \n1 torpilleur (2 cases)."
-                   "\nLe but du jeu est de rentrer des coordonnee (par example 4 et 7) "
-                   "\net d'essayer de toucher les bateaux adverses. En tirant le moins de coup possible.\n\n");
-        }
-        if(carteJoueur[PosV][PosH] > 0){
+
+        //demande les positions ou affiche l'aide ou arrête le programme selon la volontée de l'utilisateur
+        do {
+            //affiche le nombre de bateau coulé au total
+            printf("\nbateau coulé :%d",bateauCoule);
+            //demande d'entré des position
+            printf("\n\nEntrer la position de tir");
+            printf("\n(Pour quitter taper 11. Ou pour de l'aide taper 12.)");
+            printf("\n\nPosition Horizontal:");
+            scanf("\n%d", &PosH);
+            printf("\nPosition Vertical:");
+            scanf("%d", &PosV);
+            PosH -= 1;
+            PosV -= 1;
+            if ((PosV == 10) || (PosH == 10)) {
+                exit(EXIT_SUCCESS);
+            } else if ((PosV == 11) || (PosH == 11)) {
+                printf("Voici l'aide du jeu:");
+                printf("\n\nLe jeu possede 5 navires: "
+                       "\n1 portes avion (5 cases),"
+                       "\n1 croiseur (4 cases), "
+                       "\n2 contre-torpilleurs (3 cases),"
+                       "\n1 torpilleur (2 cases)."
+                       "\nLe but du jeu est de rentrer des coordonnee (par example 4 et 7) "
+                       "\net d'essayer de toucher les bateaux adverses. En tirant le moins de coup possible."
+                       "\nLe jeu affiche 1 si vous ratez, un 2 si vous touchez et 3 si vous faites touché-coulé.\n");
+            }
+            //affiche un texte si la position à déjà était rentrée
+            if(carteJoueur[PosV][PosH] > 0){
             printf("\nDéja choisit");
+            }
+        } while ((PosH < 0) || (PosH > 9) || (PosV < 0) || (PosV > 9) || (carteJoueur[PosV][PosH] > 0));
+
+        //affiche raté ou coulé si la position contient un bateau ou pas
+        if (carteBateau[PosV][PosH] == 0) {
+            printf("\n------"
+                   "\nRaté !"
+                   "\n------\n");
+            carteJoueur[PosV][PosH] = 1;
+        } else if (carteBateau[PosV][PosH] = !0) {
+            printf("\n--------"
+                   "\nTouché !"
+                   "\n--------\n");
+            carteJoueur[PosV][PosH] = 2;
         }
-    } while ((PosH < 0) || (PosH > 9) || (PosV < 0) || (PosV > 9) || (carteJoueur[PosV][PosH] > 0));
 
-
-    if (carteBateau[PosV][PosH] == 0) {
-        printf("\n------"
-               "\nRaté !"
-               "\n------\n");
-        carteJoueur[PosV][PosH] = 1;
-    } else if (carteBateau[PosV][PosH] = !0) {
-        printf("\n--------"
-               "\nTouché !"
-               "\n--------\n");
-        carteJoueur[PosV][PosH] = 2;
-    }
-
-    if((carteJoueur[3][3] == 2) && (carteJoueur[3][4] == 2)){
-        printf("\nCoulé");
-        carteJoueur[3][3]  = 3;
-        carteJoueur[3][4]  = 3;
-        bateauCoule += 1;
-    }
-
-    if((carteJoueur[1][1] == 2) && (carteJoueur[1][2] == 2) && (carteJoueur[1][3] == 2)){
-        printf("\nCoulé");
-        carteJoueur[1][1]  = 3;
-        carteJoueur[1][2]  = 3;
-        carteJoueur[1][3]  = 3;
-        bateauCoule += 1;
-    }
-
+        //affiche coulé si le bateau a été entièrement touché
+        //bateau 1
+        if((carteJoueur[3][3] == 2) && (carteJoueur[3][4] == 2)){
+            printf("Coulé"
+                   "\n-----");
+            carteJoueur[3][3]  = 3;
+            carteJoueur[3][4]  = 3;
+            bateauCoule += 1;
+        }
+        //bateau 2
+        if((carteJoueur[1][1] == 2) && (carteJoueur[1][2] == 2) && (carteJoueur[1][3] == 2)){
+            printf("Coulé"
+                   "\n-----");
+            carteJoueur[1][1]  = 3;
+            carteJoueur[1][2]  = 3;
+            carteJoueur[1][3]  = 3;
+            bateauCoule += 1;
+        }
+        //bateau 3
         if((carteJoueur[6][4] == 2) && (carteJoueur[7][4] == 2) && (carteJoueur[8][4] == 2)){
-            printf("\nCoulé");
+            printf("Coulé"
+                   "\n-----");
             carteJoueur[6][4]  = 3;
             carteJoueur[7][4]  = 3;
             carteJoueur[8][4]  = 3;
             bateauCoule += 1;
         }
-
+        //bateau 4
         if((carteJoueur[4][7] == 2) && (carteJoueur[5][7] == 2) && (carteJoueur[6][7] == 2) && (carteJoueur[7][7] ==2)){
-            printf("\nCoulé");
+            printf("Coulé"
+                   "\n-----");
             carteJoueur[4][7]  = 3;
             carteJoueur[5][7]  = 3;
             carteJoueur[6][7]  = 3;
             carteJoueur[7][7]  = 3;
             bateauCoule += 1;
         }
-
+        //bateau 5
         if((carteJoueur[5][2] == 2) && (carteJoueur[6][2] == 2) && (carteJoueur[7][2] == 2) && (carteJoueur[8][2] ==2)
         && (carteJoueur[9][2] ==2)){
-            printf("\nCoulé");
+            printf("Coulé"
+                   "\n-----");
             carteJoueur[5][2]  = 3;
             carteJoueur[6][2]  = 3;
             carteJoueur[7][2]  = 3;
@@ -157,14 +165,17 @@ int main() {
             carteJoueur[9][2]  = 3;
             bateauCoule += 1;
         }
+    //termine la boucle si tout les bateaux ont été coulé
+    }while(bateauCoule < 5);
 
+    //petit texte de victoire à la fin
+    printf("\n----------"
+           "\nGagner !!!"
+           "\n----------"
+           "\nBien joué."
+           "\n----------\n\n");
 
-
-}while(bateauCoule < 5);
-
-    printf("\ngagner");
-
-    system("\nPause");
+    system("Pause");
 
     return 0;
 }
